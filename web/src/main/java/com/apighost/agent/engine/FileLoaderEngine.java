61b9910a3897413c603743ee1140fca7d1623eca
영석 <149328708+kobenlys@users.noreply.github.com>
2025-05-07 02:44:42 +0900

    feat: improve of fault tolerance
    The implementation has been updated to ensure that a failure during JSON parsing does not interrupt the overall processing flow.

--- web/src/main/java/com/apighost/agent/engine/FileLoaderEngine.java
+++ web/src/main/java/com/apighost/agent/engine/FileLoaderEngine.java
@@ -1,5 +1,6 @@
 package com.apighost.agent.engine;
 
+import com.apighost.agent.executor.ScenarioTestExecutor;
 import com.apighost.agent.loader.FileLoader;
 import com.apighost.agent.model.ScenarioResultBrief;
 import com.apighost.agent.model.ScenarioResultListResponse;
@@ -10,11 +11,14 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FileLoaderEngine {
 
     private final FileLoader fileLoader;
     private final ObjectMapper objectMapper;
+    private static final Logger log = LoggerFactory.getLogger(ScenarioTestExecutor.class);
 
     public FileLoaderEngine(FileLoader fileLoader, ObjectMapper objectMapper) {
         this.fileLoader = fileLoader;
@@ -29,19 +33,22 @@ public class FileLoaderEngine {
         List<ScenarioResultBrief> resultBriefs = new ArrayList<>();
         List<File> resultFiles = fileLoader.getScenarioResults();
 
-        try {
-            for (File file : resultFiles) {
-                ScenarioResult scenarioResult = objectMapper.readValue(file, ScenarioResult.class);
+        for (File file : resultFiles) {
+            ScenarioResult scenarioResult;
+            try {
+                scenarioResult = objectMapper.readValue(file, ScenarioResult.class);
 
-                ScenarioResultBrief brief = new ScenarioResultBrief(scenarioResult.getName(),
-                    scenarioResult.isScenarioSuccess(), scenarioResult.getExecutedAt());
-                resultBriefs.add(brief);
+            } catch (IOException e) {
+                log.error("Failed to parse result file: {}", file.getAbsolutePath());
+                continue;
             }
 
-            return new ScenarioResultListResponse(resultBriefs);
+            ScenarioResultBrief brief = new ScenarioResultBrief(scenarioResult.getName(),
+                scenarioResult.isScenarioSuccess(), scenarioResult.getExecutedAt());
 
-        } catch (IOException e) {
-            throw new IllegalStateException("An error occurred while parsing the JSON");
+            resultBriefs.add(brief);
         }
+
+        return new ScenarioResultListResponse(resultBriefs);
     }
 }
